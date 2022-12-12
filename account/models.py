e4b7079580db6418d4dec863cfe2223d3d9edb47
Mr. X <mohebmoallem1376@gmail.com>
2022-12-12 15:00:43 +0530

    delete user verify model

--- account/models.py
+++ account/models.py
@@ -1,9 +1,4 @@
-from django.db import models
 from django.contrib.auth.models import User
 
 User._meta.get_field('email')._unique = True
 
-
-class UserVerify(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    code = models.PositiveIntegerField(default=0)
